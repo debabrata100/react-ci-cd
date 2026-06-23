18bac14dbac28803e38c7de0476b9a4b6f20215b
Debabrata <m.debabrata100@gmail.com>
2026-06-23 17:36:47 +0530

    Added test and build in ci/cd

---
+++ src/utils/__tests__/math.test.js
@@ -0,0 +1,19 @@
+import { describe, it, expect } from "vitest";
+import { add, divide } from "../math";
+
+describe("add test goes here", () => {
+  it("should add two numbers", () => {
+    const result = add(2, 3);
+    expect(result).toBe(5);
+  });
+});
+describe("divide tests goes here", () => {
+  it("should divide two numbers and return the result", () => {
+    const result = divide(10, 2);
+    expect(result).toBe(5);
+  });
+  it("should throw error when diving by zero", () => {
+    const divideByZero = () => divide(10, 0);
+    expect(divideByZero).toThrowError("Cannot divide by zero");
+  });
+});
