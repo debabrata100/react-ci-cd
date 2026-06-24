d71cb6326e1736695bc63c850162dbb194459dc2
Debabrata <m.debabrata100@gmail.com>
2026-06-24 19:06:06 +0530

    Test workflow for docker cache reuse by chaning source code program fiels

--- src/App.jsx
+++ src/App.jsx
@@ -38,20 +38,6 @@ function App() {
           </svg>
           <h2>Documentation</h2>
           <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank" rel="noreferrer">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank" rel="noreferrer">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
         </div>
         <div id="social">
           <svg className="icon" role="presentation" aria-hidden="true">
