6e1c85bd2f1d0b413b926eed503b5870d6e512b5
Debabrata <m.debabrata100@gmail.com>
2026-07-02 15:12:17 +0530

    coverage issues on sonar cloud must be fixed

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,10 @@ export default defineConfig({
     environment: "jsdom",
     setupFiles: "./src/setupTests.js",
     globals: true,
+
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "html", "lcov"],
+    },
   },
 });
